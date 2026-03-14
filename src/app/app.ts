ed60ab13169157b2367515227d762d3bb061b8f6
nikolozi <Nikolozikhiblahvili@gmail.com>
2026-03-14 22:47:08 +0400

    stt

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
 
